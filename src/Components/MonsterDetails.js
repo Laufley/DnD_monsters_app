26a96d0bc47be96470d050e9d245cb9f347aa68e
Indira Borras <borrasindira@gmail.com>
2023-01-21 18:17:21 +0000

    details section now renders empty and it's filled with data upon click on item

--- src/Components/MonsterDetails.js
+++ src/Components/MonsterDetails.js
@@ -1,17 +1,22 @@
 import React from 'react'
 
-const MonsterDetails = ({details}) => {
+const MonsterDetails = ({details = {}}) => {  // setting it to an empty object so that the Details section renders empty
 
     const imgLink = `https://www.dnd5eapi.co${details.image}`
 
 
-    return (
-        <div>
-            <p>MonsterDetails: {details.name}</p>
-            <p>Type:{details.type} - {details.alignment} - {details.size}  </p>
-            <p>Stats: {details.strength}`, Dexterity: {details.dexterity}, Constitution: {details.constitution}, Intelligence: {details.intelligence}, Wisdom: {details.wisdom}, Charisma: {details.charisma}</p>
-            <img src={imgLink}  height="300px" width="300px"/>
+    return ( 
+        <div> 
+            {details && details.name &&  // had to add 2 logical conditions (because just for null and undefined wasn't enough))
+            <>
+                <p>MonsterDetails: {details.name}</p>
+                <p>Type:{details.type} - {details.alignment} - {details.size}  </p>
+                <p>Stats: {details.strength}`, Dexterity: {details.dexterity}, Constitution: {details.constitution}, Intelligence: {details.intelligence}, Wisdom: {details.wisdom}, Charisma: {details.charisma}</p>
+                <img src={imgLink}  height="300px" width="300px"/>
+            </>
+        }
         </div>
+        
     )
     
     
