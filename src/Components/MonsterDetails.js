aba2abebc4da06900959f66b4be12ace518a04aa
Indira Borras <borrasindira@gmail.com>
2023-01-21 22:26:35 +0000

    css improved more

--- src/Components/MonsterDetails.js
+++ src/Components/MonsterDetails.js
@@ -10,11 +10,25 @@ const MonsterDetails = ({details = {}}) => {  // setting it to an empty object s
             {details && details.name &&  // had to add 2 logical conditions (because just for null and undefined wasn't enough))
             <>
                 <div className='details'>
-                    <p>Monster details: {details.name}</p>
-                    <p>Type: {details.type} - {details.alignment} - {details.size}  </p>
-                    <p>Stats: {details.strength}, Dexterity: {details.dexterity}, Constitution: {details.constitution}, Intelligence: {details.intelligence}, Wisdom: {details.wisdom}, Charisma: {details.charisma}</p>
+                    <ul className='stats'>
+                        <li><strong>Name:</strong> {details.name} </li>
+                        <li><strong>Type: </strong>{details.type}</li>
+                        <li><strong>Alignment: </strong>{details.alignment}  </li>
+                        <li><strong>Size: </strong>{details.size} </li>
+                        <li><strong>Stats:</strong>  
+                            <ul>
+                                <li><strong>Strength:</strong>{details.strength}</li>
+                                <li><strong>Dexterity:</strong>{details.dexterity}</li>
+                                <li><strong>Constitution:</strong>{details.constitution}</li>
+                                <li><strong>Intelligence:</strong>{details.intelligence}</li>
+                                <li><strong>Wisdom: </strong>{details.wisdom}</li>
+                                <li><strong>Charisma:</strong>{details.charisma}</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <img className='image' src={imgLink}  height="300px" width="300px"/>
                 </div>
-                <img className='image' src={imgLink}  height="300px" width="300px"/>
+                
             </>
         }
         </div>
