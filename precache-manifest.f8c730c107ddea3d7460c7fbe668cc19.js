351dc521c822a991657120b95cd8514e76fabe9b
Indira Borras Gonzalez <ibg.indira@gmail.com>
2023-05-09 11:46:57 +0100

    Updates

---
+++ precache-manifest.f8c730c107ddea3d7460c7fbe668cc19.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4eece61a812a6fcc23de",
+    "url": "/monster_archives_app/static/css/main.d4643fb6.chunk.css"
+  },
+  {
+    "revision": "4eece61a812a6fcc23de",
+    "url": "/monster_archives_app/static/js/main.4eece61a.chunk.js"
+  },
+  {
+    "revision": "bfc464742c84ccff5d43",
+    "url": "/monster_archives_app/static/js/1.bfc46474.chunk.js"
+  },
+  {
+    "revision": "ff0446a81261167c6f97",
+    "url": "/monster_archives_app/static/js/2.ff0446a8.chunk.js"
+  },
+  {
+    "revision": "cff443c351ad1f6c21c4",
+    "url": "/monster_archives_app/static/js/runtime~main.cff443c3.js"
+  },
+  {
+    "revision": "e23bb253ec1ff52e34abdc21fb5a3ea9",
+    "url": "/monster_archives_app/static/media/dummy.e23bb253.jpeg"
+  },
+  {
+    "revision": "cccf07982aa3710532a8f7b9925fd3a8",
+    "url": "/monster_archives_app/static/media/music.cccf0798.jpeg"
+  },
+  {
+    "revision": "bee5ae93c426d9feeb10d7c5fc55920b",
+    "url": "/monster_archives_app/static/media/ancient-wind-112528.bee5ae93.mp3"
+  },
+  {
+    "revision": "64daf639bd4f62ee634309e6e4d3f0e2",
+    "url": "/monster_archives_app/static/media/electronic-rock-king-around-here-15045.64daf639.mp3"
+  },
+  {
+    "revision": "99c2f2e0bc0b9db3ca58fa5e45dfff06",
+    "url": "/monster_archives_app/static/media/dragon.99c2f2e0.jpeg"
+  },
+  {
+    "revision": "30f6b014f2f096034b4372dd9fb8bbe7",
+    "url": "/monster_archives_app/static/media/red.30f6b014.png"
+  },
+  {
+    "revision": "af13967daf01197afda79e01e008b5d0",
+    "url": "/monster_archives_app/index.html"
+  }
+];
